762eed926aca1dfbd779dc1c6faec265fe78e769
Harlem Kirua <debabel1hackamani@gmail.com>
2024-11-02 18:03:31 +0000

    first commit

---
+++ app.ts
@@ -0,0 +1,7 @@
+// select dom element
+const form = document.getElementById("todo-form") as HTMLElement
+const input = document.getElementById("todo-input") as HTMLElement
+const todoList = document.getElementById("todo-list") as HTMLElement
+
+// event
+
